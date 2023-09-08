5e8c895c6d3db4cf250a07d004879ac284ac2480
Smart-SangGe <2251250136@qq.com>
2023-09-08 10:13:23 +0800

    删除无用依赖

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from hashlib import sha256
-from operator import methodcaller
-from flask import Flask, render_template, redirect, url_for, flash, request, session
+from flask import Flask, get_flashed_messages, render_template, redirect, url_for, flash, request, session, jsonify
 from flask_wtf import FlaskForm
 from flask_socketio import SocketIO
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Regexp
-from flask_login import LoginManager, current_user, login_required, login_user, UserMixin
-from db_model import db, User, Message, FriendRequest
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from db_model import db, User, Message, FriendRequest, friendship
 import sqlalchemy.exc
 
 
@@ -53,6 +52,7 @@ def register():
         try:
             db.session.add(user)
             db.session.commit()
+            flash("注册成功")
             return redirect(url_for('login'))
         except sqlalchemy.exc.DBAPIError:
             db.session.rollback()
@@ -68,19 +68,30 @@ class LoginForm(FlaskForm):
 # 登录页
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    data = get_flashed_messages()
+    print(data)
     form = LoginForm()
-    if form.validate_on_submit():
-        encpass = sha256(form.password.data.encode()).hexdigest()
-        user = User.query.filter_by(username=form.username.data).first()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            encpass = sha256(form.password.data.encode()).hexdigest()
+            user = User.query.filter_by(username=form.username.data).first()
+            if user and user.password == encpass:
+                # 如果用户存在并且密码正确
+                login_user(user)
+                flash(f"欢迎回来，{user.username}!")
+                return redirect(url_for('chat'))
+            else:
+                flash('登录失败。请检查用户名和密码是否正确。', 'danger')
         
-        if user and user.password == encpass:
-            # 如果用户存在并且密码正确
-            login_user(user)
-            return redirect(url_for('chat')) 
-        else:
-            flash('登录失败。请检查用户名和密码是否正确。', 'danger')
     return render_template('login.html', title='Login', form=form)
 
+# 处理消息
+@socketio.on('send_message')
+def handle_message(data):
+    message = data['message']
+    socketio.emit('new_message', {'message': message})
+    
+    
 # 聊天页
 @app.route('/chat', methods=['GET', 'POST'])
 @login_required
@@ -88,8 +99,99 @@ def chat():
     
     return render_template("chat.html", title="ChatRoom")
 
+# 添加好友
+@app.route('/sendFriendRequest', methods=['POST'])
+def sendFriendRequest():
+    friend_username = request.form.get('username')
+    
+    # 查询数据库，查找该 username 对应的用户
+    friend = User.query.filter_by(username=friend_username).first()
+    
+    if not friend:
+        return jsonify(status="error", error="该用户不存在")
+
+    # 创建好友请求记录
+    # 这里我们假设你已经有了当前登录的用户的信息
+    current_user = current_user()
+    
+    friend_request = FriendRequest(sender_id=current_user.id, receiver_id=friend.id)
+    db.session.add(friend_request)
+    db.session.commit()
+    
+    return jsonify(status="success", message="好友请求已发送")
+
+# 好友申请列表
+@app.route()
+def friendRequests():
+    current_user = current_user()
+
+    # 从数据库中获取所有未处理的好友请求
+    pending_requests = FriendRequest.query.filter_by(receiver_id=current_user.id).all()
+    
+    # 这里可以直接将它们传递给前端模板来展示
+    return render_template('friend_requests.html', requests=pending_requests)
+
+# 同意好友申请
+@app.route('/acceptFriendRequest', methods=['POST'])
+def acceptFriendRequest():
+    request_id = request.form.get('requestId')
+    
+    # 从数据库中找到这个好友请求
+    friend_request = FriendRequest.query.get(request_id)
+
+    if not friend_request:
+        return jsonify(status="error", error="请求不存在")
+
+    # 获取双方的用户对象
+    sender = User.query.get(friend_request.sender_id)
+    receiver = User.query.get(friend_request.receiver_id)
+    
+    if not sender or not receiver:
+        return jsonify(status="error", error="用户不存在")
+
+    # 在friendship表中创建新的记录
+    # 添加第一条记录
+    db.session.add(friendship(user_id=sender.id, friend_id=receiver.id))
+    # 添加第二条记录
+    db.session.add(friendship(user_id=receiver.id, friend_id=sender.id))
+
+    # 删除原始的FriendRequest记录
+    db.session.delete(friend_request)
+
+    # 提交数据库更改
+    db.session.commit()
+
+    return jsonify(status="success", message="好友已添加")
+
+# 拒绝好友申请
+@app.route('/rejectFriendRequest', methods=['POST'])
+def rejectFriendRequest():
+    request_id = request.form.get('requestId')
+    
+    # 从数据库中找到这个好友请求
+    friend_request = FriendRequest.query.get(request_id)
+
+    if not friend_request:
+        return jsonify(status="error", error="请求不存在")
+
+    # 删除原始的FriendRequest记录
+    db.session.delete(friend_request)
+
+    # 提交数据库更改
+    db.session.commit()
+
+    return jsonify(status="success", message="已拒绝好友请求")
+
+
+# 退出登录
+@app.route('/logout', method=['GET'])
+def logout():
+    logout_user()
+    return redirect(url_for('login'))  # 重定向到登录页面或主页
+
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # 创建数据库表
-    app.run(debug=True)
+    # app.run(debug=True)
+    socketio.run(app, debug=True)
