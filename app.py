eaf887acf619e9efaea55fc080f9c60b0cf7b87e
Smart-SangGe <2251250136@qq.com>
2023-09-11 16:29:05 +0800

    移除无用依赖

--- app.py
+++ app.py
@@ -293,7 +293,5 @@ def logout():
 
 
 if __name__ == '__main__':
-    # with app.app_context():
-    #     db.create_all()  # 创建数据库表
-    # # app.run(debug=True)
+    
     socketio.run(app, host="0.0.0.0", debug=True)
