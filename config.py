e290ceb13eaca9a8dcd77c3e584ebc8808910559
Smart-SangGe <2251250136@qq.com>
2023-09-21 11:34:06 +0800

    改用mariadb

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 APP_VERSION = "0.0.3"
 SECRET_KEY = 'a_secure_key'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskuser:flaskpassword@mariadb:3306/flaskdb'
